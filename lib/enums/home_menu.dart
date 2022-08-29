ea33ad21ad059b9e8c12ad5792f5875dcdbab06e
mamunhossain <mh72156@gmail.com>
2022-08-30 02:08:42 +0600

    camelCase warning fix

--- lib/enums/home_menu.dart
+++ lib/enums/home_menu.dart
@@ -2,36 +2,33 @@ import 'package:bkash/styles/AppText.dart';
 import 'package:bkash/widgets/send_money_widget.dart';
 import 'package:flutter/cupertino.dart';
 
-import '../widgets/offer_widget.dart';
-
 enum HomeMenu {
   //top menu
-  SEND_MONEY('Send Money', 'assets/send_money.jpg', SendMoneyWidget()),
-  CASH_OUT('Cash Out', 'assets/cash_out.jpg', Text(AppText.placeholder)),
-  TOP_UP('Top-up', 'assets/mobile_recharge.jpg', Text(AppText.placeholder)),
-  PAYMENT('Payment', 'assets/make_payment.jpg', Text(AppText.placeholder)),
+  sendMoney('Send Money', 'assets/send_money.jpg', SendMoneyWidget()),
+  cashOut('Cash Out', 'assets/cash_out.jpg', Text(AppText.placeholder)),
+  topUp('Top-up', 'assets/mobile_recharge.jpg', Text(AppText.placeholder)),
+  payment('Payment', 'assets/make_payment.jpg', Text(AppText.placeholder)),
 
-  ADD_MONEY('Add Money', 'assets/add_money.jpg', Text(AppText.placeholder)),
-  PAY_BILL('Pay Bill', 'assets/pay_bill.jpg', Text(AppText.placeholder)),
-  TICKETS('Tickets', 'assets/tickets.jpg', Text(AppText.placeholder)),
-  MORE('More', 'assets/more.jpg', Text(AppText.placeholder)),
+  addMoney('Add Money', 'assets/add_money.jpg', Text(AppText.placeholder)),
+  payBill('Pay Bill', 'assets/pay_bill.jpg', Text(AppText.placeholder)),
+  tickets('Tickets', 'assets/tickets.jpg', Text(AppText.placeholder)),
+  more('More', 'assets/more.jpg', Text(AppText.placeholder)),
 
   //my bKash
-  TOPUP('Evan Emran', 'assets/mobile_recharge.jpg', Text(AppText.placeholder)),
-  SHWAPNO('Shwapno', 'assets/make_payment.jpg', Text(AppText.placeholder)),
-  INTERNET('Internet', 'assets/pay_bill.jpg', Text(AppText.placeholder)),
-  CARD('Card', 'assets/add_money.jpg', Text(AppText.placeholder)),
-  ACCOUNT('My Account', 'assets/mobile_recharge.jpg', Text(AppText.placeholder)),
+  topUps('Evan Emran', 'assets/mobile_recharge.jpg', Text(AppText.placeholder)),
+  shwapno('Shwapno', 'assets/make_payment.jpg', Text(AppText.placeholder)),
+  internet('Internet', 'assets/pay_bill.jpg', Text(AppText.placeholder)),
+  card('Card', 'assets/add_money.jpg', Text(AppText.placeholder)),
+  account('My Account', 'assets/mobile_recharge.jpg', Text(AppText.placeholder)),
 
   //suggestions
-  BTCL('BTCL', 'assets/btcl.jpg', Text(AppText.placeholder)),
-  AKASH('Akash DTH', 'assets/akash.jpg', Text(AppText.placeholder)),
-  AJKER_DEAL('Ajker Deal', 'assets/ajkerdeal.jpg', Text(AppText.placeholder)),
-  DARAZ('Daraz', 'assets/daraz.jpg', Text(AppText.placeholder)),
-  STYLINE('Sty Line', 'assets/stiline.jpg', Text(AppText.placeholder)),
+  btcl('BTCL', 'assets/btcl.jpg', Text(AppText.placeholder)),
+  akash('Akash DTH', 'assets/akash.jpg', Text(AppText.placeholder)),
+  ajkelDeal('Ajker Deal', 'assets/ajkerdeal.jpg', Text(AppText.placeholder)),
+  daraz('Daraz', 'assets/daraz.jpg', Text(AppText.placeholder)),
+  styline('Sty Line', 'assets/stiline.jpg', Text(AppText.placeholder)),
 
-
-  MOREE('More', 'assets/more.jpg', Text(AppText.placeholder));
+  moree('More', 'assets/more.jpg', Text(AppText.placeholder));
 
 
   final String title;
